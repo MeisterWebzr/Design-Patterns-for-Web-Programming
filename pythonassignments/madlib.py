c646573f7e3de493faa7120bd7750a0761421b4d
MesiterWebzr <meister@fullsail.edu>
2014-08-07 13:22:52 -0700

    finalized
    commenting on all sections for submission to fso

--- pythonassignments/madlib.py
+++ pythonassignments/madlib.py
@@ -1,8 +1,14 @@
+'''
+Student: Otto "Meister" Burroughs
+Date: 8-7-14
+Class: DPWA- online
+Assignment: madlib
 
+'''
 # Opener for madlib super hero game
 #String 1
 superhero = raw_input("Enter your Superhero's name:  ")
-#raw impu for superhero age
+#raw input for superhero age
 age = raw_input("Enter your Superhero's age: ")
 #asking sex of super hero
 sex = raw_input("Is your Superhero male or female?  ")
@@ -16,24 +22,27 @@ time = raw_input("How long did it take for your SuperHero to master his powers? 
 #raw input for flaw question
 flaw = raw_input("All Superhero's have flaws, so what flaw does your Superhero have. This can be the power itself or "
                  "something simple like rabbits paralyze his legs or blind him for 10 seconds.  ")
-
+#raw input for personality question
 personality = raw_input("What type of personality does your super hero have? ie; Bully, shy, pompous or witty and "
-                        "energetic.  ")
-outfits = raw_input("How many villians does your Superhero have?  ")
+                     "energetic.  ")
+#raw input for personality question
+outfits = raw_input("How many villains does your Superhero have?  ")
 
-def calcArea(h, w):
-        area = h * w
-        return area
-a = calcArea(210, 122);
+#Functoin calculator to calculate speed of super hero
+def calcSpeed(t, d):
+        speed = t / d #speed is time / distance
+        return speed #return speed results to s
+s = calcSpeed(1210, 21); #dividing speed
 
+#printing entire story
 print superhero + " is " + age + " "+  sex + ". With " + power + " ability and " + outfits +\
       " x's " + personality + " of a regular human. "+ " Flaw: " + flaw + ". " + superhero +" " + how + \
-      ". But, flying through at " + str(a) + " mph wicked fast. " + superhero + " is now entered into the Hall of" \
+      ". But, flying through at " + str(s) + " mph wicked fast. " + superhero + " is now entered into the Hall of" \
                                                                                 " Fame, see details below."
 
-#"FOR EACH Supehero hall of fame" LOOP
-halloffame = ["Superman 2010", "Wonder Woman 2011","Batman 2012", "Meister Webzr 2013", superhero + " 2014"]
-for r in halloffame:
-    print "W3C Hall of Fame winner " + r
-    pass
-print "thank you for creating a super hero with Madlib. Type clear and hit ENTER"
+#"FOR EACH Superhero hall of fame" LOOP
+halloffame = ["Superman 2010", "Wonder Woman 2011","Batman 2012", "Meister Webzr 2013", superhero + " 2014"] #array of superhero's
+for r in halloffame:#setting r to cycle through the super hero array
+    print "W3C Hall of Fame winner " + r #prinitng the results of array and string
+    pass#passing to lower line
+print "thank you for creating a super hero with Madlib. Type clear and hit ENTER"#printing thank you for playing game
