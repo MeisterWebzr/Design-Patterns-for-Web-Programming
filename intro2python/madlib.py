3625a2362d852d01236adce96317124d11a89e30
MesiterWebzr <meister@fullsail.edu>
2014-08-06 21:57:43 -0700

    madlib coding
    1st string and conditional statement 1

--- intro2python/madlib.py
+++ intro2python/madlib.py
@@ -1,5 +1,12 @@
 
 #Opener for madlib super hero game
+#String 1
 superhero = raw_input("Enter your Superhero's name  ")
+
+#conditional statement
+if age < 20:
+    #stuff if condition is true
+    power = "Animation"
+    print superhero + " Your power is " + power + ". Also known as Thought Projection, is the power to bring inanimate objects to life or to free a person from petrification. Mr. Mxyzptlk used this power on a mannequin, a statue, and even the Daily Planet."
 
 
