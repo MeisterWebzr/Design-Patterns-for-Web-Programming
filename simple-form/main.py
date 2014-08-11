1026dce0631606809ada450eec5538ce244cbead
MesiterWebzr <meister@fullsail.edu>
2014-08-11 13:05:46 -0700

    Form
    editing form inputs for page_body

--- simple-form/main.py
+++ simple-form/main.py
@@ -9,16 +9,48 @@ import webapp2
 class MainHandler(webapp2.RequestHandler):
     def get(self):
 
-        page_head = '''<!DOCTYPE HTML>
+        page_head = '''<!DOCTYPE HTML><!-- Begin HTML -->
 <html>
     <head>
         <link rel="stylesheet" href="css/main.css">
-        <title>Web work Request</title>
+        <title>Web work request</title>
     </head>
     <body>'''
 
         page_body = '''
+            <div id="ad"><h1>Summer orders get 25% off</h1></div>
+            <fieldset>Summer orders get 25% off</fieldset>
             <form>
+            <div class="row"><label>First name:</label><input type="text" name="firstname"></div>
+            <div class="row"><label>Last name:</label><input type="text" name="lastname"></div>
+            <div class="row"><label>Email main:</label><input type="text" name="email"></div>
+            <div class="row"><label>Best Contact#:</label><input type="tel" name="tel"></div>
+            <div class="row">
+                <label>Project Type:</label>
+                    <select name="project" required>
+                        <option value="Graphic Design">Graphic Design</option>
+                        <option value="Web Design">Web Design</option>
+                        <option value="Prototype Application">Prototype Application</option>
+                        <option value="Web Development">Web Development</option>
+                        <option value="Network / Hosting">Network / Hosting</option>
+                        <option value="Mobile App Design">Mobile App Design</option>
+                        <option value="Monthly Web Maintenance">Monthly Web Maintenance</option>
+                        <option value="Wireframes / Mockups">Wireframes / Mockups</option>
+                        <option value="Company Branding">Company Branding</option>
+                        <option value="Hosting">Hosting</option>
+                        <option value="Ecommerce Setup">Ecommerce Setup</option>
+                    </select>
+            </div>
+            <div class="row">
+
+                <input type="checkbox" name"Terms" value="Terms">You agree to the above information being valid ans true.
+
+            </div>
+
+
+
+
+            <input id="sub" type="submit" name="" value="Submit">
 
         '''
 
