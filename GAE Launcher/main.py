3ac43b89a85c81b11c6f45b27bd264287292032f
MesiterWebzr <meister@fullsail.edu>
2014-08-21 15:00:53 -0700

    Import
    imported url urllib2 also setup
    from xml.etree.ElementTree import QName
    import  xml.etree.ElementTree as ET

--- GAE Launcher/main.py
+++ GAE Launcher/main.py
@@ -1,21 +1,17 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+'''
+Student: Otto Burroughs
+Date: 8-21-2014
+Assignment: GEA Launcher
+'''
 import webapp2
+import urllib2#here we import all the classes and code needed to request info, receive that info and open info
+from xml.etree.ElementTree import QName #from xml.etree dir we setup communication
+import xml.etree.ElementTree as ET #import library classes and syntax and setting naming of ET in application
 
+
+
+
+#never touch info below
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
