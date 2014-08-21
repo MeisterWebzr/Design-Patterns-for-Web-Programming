e2bac185abd66b2989ee71c6b37f2c9027e1057f
MesiterWebzr <meister@fullsail.edu>
2014-08-21 15:14:23 -0700

    HTML page
    setup

--- GAE Launcher/main.py
+++ GAE Launcher/main.py
@@ -17,20 +17,30 @@ class MainHandler(webapp2.RequestHandler):
 #this will serve as our abstract class with no instances
 class Page(object):
     def __init__(self):#constructor function to call functions below
+        #setting title of application
         self.title = "News 4 U!"
+        #setting stylesheet directory
         self.css = "css/main.css"
+        #head section of html setup
         self.head = '''
 <!DOCTYPE HTML>
 <html>
     <head><title></title></head>
     <body>'''
+        #body section where all content will go
         self.body = 'Search top news'
+        #closing function to wrap end tag of html to print page to browser
         self.close = '''
     </body>
 </html>'''
 
 
-    
+    def print_out(self):#funtion to print out above page
+        return  self.head + self.body + self.close# calling and printing all above functions to print html page
+
+
+
+
 
 
 
